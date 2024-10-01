a0f1ca682852bb4ff8d09b33df5f76f051b4ff98
catherineumali <catherineumali@gmail.com>
2024-10-01 14:04:13 +0800

    Add mui, add form

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,38 @@
-import { useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useState } from "react";
+import Box from "@mui/joy/Box";
+import Button from "@mui/joy/Button";
+import Textarea from "@mui/joy/Textarea";
+
 import "./App.css";
 
-import OpenAI from "openai";
-
-const apiUrl = process.env.REACT_APP_API_URL;
-
-function App() {
-  const [count, setCount] = useState(0);
-
-  useEffect(() => {
-    async function main() {
-      const openai = new OpenAI({
-        apiKey: apiUrl,
-        dangerouslyAllowBrowser: true,
-      });
-
-      const completion = await openai.chat.completions.create({
-        messages: [
-          { role: "system", content: "You are a helpful assistant." },
-          { role: "user", content: "What is a LLM?" },
-        ],
-        model: "gpt-4o-mini",
-      });
-
-      console.log(completion.choices[0]);
-    }
-    main();
-  }, []);
-
+export default function App() {
+  const [inputValue, setInputValue] = useState("");
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Box
+      sx={{
+        py: 2,
+        display: "flex",
+        flexDirection: "column",
+        gap: 2,
+        alignItems: "center",
+        flexWrap: "wrap",
+      }}
+    >
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          console.log(inputValue);
+        }}
+      >
+        <Textarea
+          placeholder="Ask anything!"
+          required
+          sx={{ mb: 1 }}
+          minRows={4}
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Box>
   );
 }
-
-export default App;
